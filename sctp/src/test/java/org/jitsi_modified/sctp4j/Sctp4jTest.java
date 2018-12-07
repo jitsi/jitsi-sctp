fe8c1a85e9bf4301d780b879cc7ef4514259d762
Brian Baldino <bbaldino@gmail.com>
2018-12-07 11:13:13 -0800

    very basic test to verify things are linked up correctly

---
+++ sctp/src/test/java/org/jitsi_modified/sctp4j/Sctp4jTest.java
@@ -0,0 +1,12 @@
+package org.jitsi_modified.sctp4j;
+
+import org.junit.Test;
+
+public class Sctp4jTest {
+
+    @Test
+    public void testInit() {
+        // Very basic test to verify that everything has been linked up correctly
+        Sctp4j.init();
+    }
+}
